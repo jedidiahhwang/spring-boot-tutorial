09c5aa5ace1d7ee04bcc9e576cf6fa943636a682
Jedidiah <jedidiah.hwang.dev@gmail.com>
2022-07-06 10:51:12 -0600

    refactor business layer

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,15 +3,10 @@ package com.example.demo;
 import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController // Provides the ability for this class to serve RESTful endpoints.
+
 
 public class DemoApplication {
 
@@ -19,9 +14,4 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping // Same as a GET request.
-	public List<Student> hello() {
-		return List.of(new Student(1L, "Jeddy", "jhwang@gmail.com", LocalDate.of(1995, Month.JULY, 18), 26)); // This will automatically parse it into JSON.
-
-	}
 }
--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,20 @@
 package com.example.demo.student;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
 public class StudentController {
 
+    @GetMapping // Same as a GET request.
+    public List<Student> getStudents() {
+        return List.of(new Student(1L, "Jeddy", "jhwang@gmail.com", LocalDate.of(1995, Month.JULY, 18), 26)); // This will automatically parse it into JSON.
+
+    }
 }
